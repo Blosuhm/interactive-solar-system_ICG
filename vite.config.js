f0cf16c58be37987dd39f00aa86d72c88db46ad4
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-03-16 19:39:07 +0000

    Setup GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   server: { allowedHosts: ["laptop.neko-nominal.ts.net"] },
-};
+  base: "/interactive-solar-system_ICG/",
+});
